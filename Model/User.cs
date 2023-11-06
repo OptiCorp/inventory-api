d367cb7d6f43576c86dbced2430e8501b88e3565
Eirik Bakkan <eirikbakkan@gmail.com>
2023-11-06 16:11:07 +0100

    user handler

--- Model/User.cs
+++ Model/User.cs
@@ -3,10 +3,53 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Inventory.Models
 {
+    public enum UserStatus
+    {
+        [Display(Name = "Active")]
+        Active,
+        [Display(Name = "Disabled")]
+        Disabled,
+        [Display(Name = "Deleted")]
+        Deleted,
+    }
     public class User
     {
-        public string Id { get; set; }
-        public string? Role { get; set; }
-        public string? Name { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string? Id { get; set; }
+
+        public string? UmId { get; set; }
+
+        public string? AzureAdUserId { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string? UserRole { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string? FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string? LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string? Email { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string? Username { get; set; }
+
+        [Required]
+        [EnumDataType(typeof(UserStatus))]
+        public UserStatus Status { get; set; }
+
+        [Required]
+        public DateTime? CreatedDate { get; set; }
+
+        public DateTime? UpdatedDate { get; set; }
     }
 }
