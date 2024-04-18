c923dc02c0fb947c964fa7c3fb8567e2a7e0c26d
robinaasan <robin.aasan@hotmail.com>
2024-04-18 13:38:11 +0200

    used new version of contracts

--- AppSettings.cs
+++ AppSettings.cs
@@ -6,9 +6,9 @@ public abstract class AppSettings
     public static string TopicUserUpdated => "user-updated";
     public static string TopicUserDeleted => "user-deleted";
 
-    public static string TopicItemCreated => "item-created";
-    public static string TopicItemUpdated => "item-updated";
-    public static string TopicItemDeleted => "item-deleted";
+    public static string TopicItemEvent => "item-created";
+    // public static string TopicItemUpdated => "item-updated";
+    // public static string TopicItemDeleted => "item-deleted";
 
     public static string SubscriptionInventory => "inventory";
 }
